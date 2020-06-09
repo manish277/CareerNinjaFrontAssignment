592b624c4ce2379d28fef48637f0024c99a2f265
Manish <manishkumaryadav189@gmail.com>
2020-06-09 13:58:32 +0530

    Updates

---
+++ precache-manifest.9356d9bc7648e434ed5bc915fdfe7798.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b734a97862c5e083dd62abb04571726d",
+    "url": "/CareerNinjaFrontAssignment/index.html"
+  },
+  {
+    "revision": "cc7ba5d9f8795cb2ca0d",
+    "url": "/CareerNinjaFrontAssignment/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fccaeb9e8c95c70c725a",
+    "url": "/CareerNinjaFrontAssignment/static/css/main.c413e016.chunk.css"
+  },
+  {
+    "revision": "cc7ba5d9f8795cb2ca0d",
+    "url": "/CareerNinjaFrontAssignment/static/js/2.2a56df24.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/CareerNinjaFrontAssignment/static/js/2.2a56df24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fccaeb9e8c95c70c725a",
+    "url": "/CareerNinjaFrontAssignment/static/js/main.68348745.chunk.js"
+  },
+  {
+    "revision": "2acf89b1ebe0d47e9848",
+    "url": "/CareerNinjaFrontAssignment/static/js/runtime-main.bfff947b.js"
+  }
+]);
